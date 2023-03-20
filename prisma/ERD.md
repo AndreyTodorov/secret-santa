3bc3c29d6789335952d91ae4cdb76002d53d7c0d
Andrey Todorov <andrey.todorov@slr-holiday-service.bg>
2023-03-20 23:05:45 +0200

    fix: added ERD, initial migration, initial DB design

---
+++ prisma/ERD.md
@@ -0,0 +1,67 @@
+```mermaid
+erDiagram
+	PartyStatus {
+		value DRAFT
+		value ASSEMBLED
+		value NOTIFIED
+		value READY
+		value FINISHED
+		value CANCELLED
+	}
+	User {
+		String id PK  "cuid()"
+		String name  "nullable"
+		String email  "nullable"
+		DateTime emailVerified  "nullable"
+		String image  "nullable"
+		DateTime createdAt  "now()"
+		DateTime updatedAt  "now()"
+		DateTime deletedAt  "nullable"
+	}
+	VerificationToken {
+		String identifier
+		String token
+		DateTime expires
+	}
+	Party {
+		String id PK  "cuid()"
+		DateTime date
+		DateTime notificationAt
+		String description  "nullable"
+		String budget  "nullable"
+		PartyStatus status "DRAFT"
+		DateTime createdAt  "now()"
+		DateTime updatedAt
+		DateTime deletedAt  "nullable"
+		DateTime deactivatedAt  "nullable"
+		String creatorId FK
+	}
+	Participant {
+		String id PK  "cuid()"
+		String firstName
+		String lastName  "nullable"
+		String email
+		String phone  "nullable"
+		DateTime createdAt  "now()"
+		DateTime updatedAt
+		DateTime deletedAt  "nullable"
+		String userId FK  "nullable"
+		String participantGroupId FK
+	}
+	ParticipantGroup {
+		String id PK  "cuid()"
+		String name
+		String description  "nullable"
+		DateTime createdAt  "now()"
+		DateTime updatedAt
+		DateTime deletedAt  "nullable"
+		String creatorId FK
+	}
+	User }|--|{ Participant : participant
+	Party }o--|| User : creator
+	Party }o--|| PartyStatus : "enum:status"
+	Participant }|--|{ User : user
+	Participant }o--|| ParticipantGroup : participantGroup
+	ParticipantGroup }o--|| User : creator
+
+```
---
+++ prisma/migrations/20230320205907_init/migration.sql
@@ -0,0 +1,100 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `emailVerified` DATETIME(3) NULL,
+    `image` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `deletedAt` DATETIME(3) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `VerificationToken` (
+    `identifier` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `VerificationToken_token_key`(`token`),
+    UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Party` (
+    `id` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    `notificationAt` DATETIME(3) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `budget` VARCHAR(191) NULL,
+    `status` ENUM('DRAFT', 'ASSEMBLED', 'NOTIFIED', 'READY', 'FINISHED', 'CANCELLED') NOT NULL DEFAULT 'DRAFT',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+    `deactivatedAt` DATETIME(3) NULL,
+    `creatorId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Participant` (
+    `id` VARCHAR(191) NOT NULL,
+    `firstName` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+    `userId` VARCHAR(191) NULL,
+    `participantGroupId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Participant_email_key`(`email`),
+    UNIQUE INDEX `Participant_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ParticipantGroup` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+    `creatorId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `ParticipantGroup_name_creatorId_key`(`name`, `creatorId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_ParticipantToParty` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_ParticipantToParty_AB_unique`(`A`, `B`),
+    INDEX `_ParticipantToParty_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Party` ADD CONSTRAINT `Party_creatorId_fkey` FOREIGN KEY (`creatorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Participant` ADD CONSTRAINT `Participant_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Participant` ADD CONSTRAINT `Participant_participantGroupId_fkey` FOREIGN KEY (`participantGroupId`) REFERENCES `ParticipantGroup`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ParticipantGroup` ADD CONSTRAINT `ParticipantGroup_creatorId_fkey` FOREIGN KEY (`creatorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_ParticipantToParty` ADD CONSTRAINT `_ParticipantToParty_A_fkey` FOREIGN KEY (`A`) REFERENCES `Participant`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_ParticipantToParty` ADD CONSTRAINT `_ParticipantToParty_B_fkey` FOREIGN KEY (`B`) REFERENCES `Party`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
