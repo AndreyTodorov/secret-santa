2792b38a064095c6c6235f845efebcb414d9d708
Andrey Todorov <andrey.todorov@slr-holiday-service.bg>
2023-03-28 22:59:20 +0300

    fix: layout, Topnavbar, translations added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,11 @@
 import { type NextPage } from "next";
-import Head from "next/head";
 import useTranslation from "next-translate/useTranslation";
 
 const Home: NextPage = () => {
   const { t } = useTranslation();
   const greeting = t("home:greeting");
-  const title = t("common:title");
 
-  return (
-    <>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content="Create your secret santa party" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <div>{greeting}</div>
-      </main>
-    </>
-  );
+  return <div>{greeting}</div>;
 };
 
 export default Home;
