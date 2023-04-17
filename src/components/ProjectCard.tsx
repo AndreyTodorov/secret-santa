7fcd7b773fc42639ea8699d7d25b41185951f8fd
Andrey Todorov <andrey.todorov@slr-holiday-service.bg>
2023-04-17 18:20:14 +0300

    fix: cards hover animation

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -13,18 +13,18 @@ export const ProjectCard = ({
   description,
   color,
 }: ProjectCardsProps) => {
-  let colorClasses = `bg-red-100 hover:text-red-600 focus:text-red-600`;
+  let colorClasses = `bg-red-100 hover:text-red-500 focus:text-red-600`;
   if (color === "blue") {
-    colorClasses = `bg-blue-100 hover:text-blue-600 focus:text-blue-600`;
+    colorClasses = `bg-blue-100 hover:text-blue-500 focus:text-blue-600`;
   } else if (color === "green") {
-    colorClasses = `bg-green-100 hover:text-green-600 focus:text-green-600`;
+    colorClasses = `bg-green-100 hover:text-green-500 focus:text-green-600`;
   } else if (color === "orange") {
-    colorClasses = `bg-orange-100 hover:text-orange-600 focus:text-orange-600`;
+    colorClasses = `bg-orange-100 hover:text-orange-500 focus:text-orange-600`;
   }
 
   return (
     <Link
-      className={`mt-6 w-96 rounded-xl  border-2 border-cyan-700 p-6 text-left ${colorClasses}`}
+      className={`mt-6 w-96 rounded-xl border-2 border-cyan-700 p-6 text-left shadow-lg shadow-indigo-500/50 transition hover:scale-[103%] ${colorClasses}`}
       href={href}
     >
       <h3 className="text-2xl font-bold">{label} &rarr;</h3>
