3b6a1c84a1293e8cce1989c009197e909b5888c8
Andrey Todorov <andrey.todorov@slr-holiday-service.bg>
2023-04-17 16:36:01 +0300

    fix: home image visible on mobile

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -25,11 +25,11 @@ export default function Navbar() {
     <nav className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-2 sm:static sm:inset-auto sm:mr-6 sm:pl-0">
             <div className="flex flex-shrink-0 items-center">
               <Link href="/">
                 <img
-                  className="hidden h-8 w-auto lg:block"
+                  className="h-8 w-auto "
                   src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                   alt="Your Company"
                 />
