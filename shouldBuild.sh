045770482b6e433a560a257739b911de7086be25
Andrey Todorov <andrei.m.todorov@gmail.com>
2023-04-23 13:45:25 +0300

    build(vercel): when should vercel build

---
+++ shouldBuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" && echo $VERCEL_GIT_COMMIT_MESSAGE | grep -v "chore(release)" ]] || [[ $VERCEL_GIT_COMMIT_REF" == "dev" ]] ; then
+  # Proceed with the build
+    echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
