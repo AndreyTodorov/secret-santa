ac46f92f0a6e0e38bc7c9e147dcfd8fc714bbf01
Andrey Todorov <andrei.m.todorov@gmail.com>
2023-04-23 13:48:05 +0300

    build(should ignore): should ignore

--- shouldBuild.sh
+++ shouldBuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main" && echo $VERCEL_GIT_COMMIT_MESSAGE | grep -v "chore(release)" ]] || [[ $VERCEL_GIT_COMMIT_REF" == "dev" ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" && echo $VERCEL_GIT_COMMIT_MESSAGE | grep -v "chore(release)" ]] || [[ "$VERCEL_GIT_COMMIT_REF" == "dev" ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;
