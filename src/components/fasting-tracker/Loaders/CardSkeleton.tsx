fd8b547266437d4b2db30ea96e3afb7dd0bcbbb5
Andrey Todorov <andrei.m.todorov@gmail.com>
2023-05-02 23:36:33 +0300

    fix(ui): tweaks

--- src/components/fasting-tracker/Loaders/CardSkeleton.tsx
+++ src/components/fasting-tracker/Loaders/CardSkeleton.tsx
@@ -6,14 +6,14 @@ export const CardSkeleton = ({ count }: { count: number }) => {
           return (
             <div
               key={i}
-              className={`mt-6 h-36 w-96 animate-pulse rounded-xl border-2 border-cyan-700 bg-slate-300 p-6 text-left shadow-lg shadow-zinc-500/50 transition`}
+              className={`mt-6 h-36 w-96 animate-pulse rounded-xl border-2 border-cyan-500 bg-slate-200 p-6 text-left shadow-lg shadow-zinc-500/50 transition`}
               style={{
                 animationDelay: `{${i * 0.05}}s`,
                 animationDuration: "1s",
               }}
             >
-              <h3 className="inline-block h-10 w-80 rounded-md bg-slate-400"></h3>
-              <p className="mt-2 inline-block h-8 w-64 rounded-md bg-slate-400"></p>
+              <h3 className="inline-block h-10 w-80 rounded-md bg-slate-300"></h3>
+              <p className="mt-2 inline-block h-8 w-64 rounded-md bg-slate-300"></p>
             </div>
           );
         })}
--- src/components/fasting-tracker/Loaders/DayCardSkeleton.tsx
+++ src/components/fasting-tracker/Loaders/DayCardSkeleton.tsx
@@ -6,16 +6,16 @@ export const DayCardSkeleton = ({ count }: { count: number }) => {
           return (
             <div
               key={i}
-              className="flex h-80 animate-pulse flex-col border border-gray-700 bg-gray-400 shadow shadow-gray-500/50"
+              className="flex h-80 animate-pulse flex-col border border-gray-700 bg-slate-300 shadow shadow-gray-500/50"
               style={{
                 animationDelay: `{${i * 0.05}}s`,
                 animationDuration: "1s",
               }}
             >
-              <div className="mt-16 inline-block gap-6 bg-slate-400 p-3 text-center">
-                <div className="inline-block h-16 w-full bg-gray-500 p-2"></div>
-                <div className="inline-block h-16 w-full bg-gray-500 p-2"></div>
-                <div className="inline-block h-16 w-full bg-gray-500 p-2"></div>
+              <div className="mt-16 inline-block gap-6 bg-slate-300 p-3 text-center">
+                <div className="inline-block h-16 w-full bg-gray-400 p-2"></div>
+                <div className="inline-block h-16 w-full bg-gray-400 p-2"></div>
+                <div className="inline-block h-16 w-full bg-gray-400 p-2"></div>
               </div>
             </div>
           );
